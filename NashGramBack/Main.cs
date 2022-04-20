09baace892f26219b476c0f41ee85196d36e6137
Valerij <shishov265@gmail.com>
2022-04-21 02:57:31 +0300

    add: back project

---
+++ NashGramBack/Main.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SQLite;
+
+namespace NashGramBack;
+
+public static class MainBack
+{
+    private static SQLiteConnection? DB = null;
+
+    public static void Main()
+    {
+        //ConnectToBD();
+        //Log.AddLog("Initialization was successful", false);
+
+        AccountDB.GetAccountFromID(1);
+        AccountDB.CreateAccount("login10","password4");
+    }
+
+    //public static void ConnectToBD()
+    //{
+    //    DB = new SQLiteConnection("Data Source=database.db;Version=3;");
+    //    DB.Open();
+    //}
+
+    //public static void DisconnectToBD()
+    //{
+    //    DB.Close();
+    //}
+}
