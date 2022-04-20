2f0d46b4d217559e64e26acd155aabd1a3831d1b
Valerij <shishov265@gmail.com>
2022-04-21 02:57:47 +0300

    add: Account class

---
+++ NashGramBack/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashGramBack
+{
+    public class Account
+    {
+        public long id_account;
+        public string login;
+        public string password;
+    }
+}
