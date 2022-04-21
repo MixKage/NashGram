17d0349e5a45e9eacd9338bedfd5ece3ec2b5624
Valerij <shishov265@gmail.com>
2022-04-22 00:27:38 +0300

    add: log

---
+++ NashGramAPI/Log/Log.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashGramAPI;
+
+public static class Log
+{
+    public static string LogText { get; private set; }
+    public static void AddLog(string text, bool mode)
+    {
+        string info = "INFO: ";
+        if (mode) info = "ERROR: ";
+        LogText = info + text + Environment.NewLine;
+        
+        using (System.IO.StreamWriter sw = new System.IO.StreamWriter("LogFile.txt", true, Encoding.Default))
+        {
+            sw.Write(LogText);
+        }
+    }
+    public static void SaveLogFile()
+    {
+        using (System.IO.StreamWriter sw = new System.IO.StreamWriter("LogFile.txt", false, Encoding.Default))
+        {
+            sw.Write(LogText);
+        }
+    }
+}
