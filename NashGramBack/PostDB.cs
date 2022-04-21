cefc586f3b9aa8baf3540a7c79694ea3e817ce4c
Valerij <shishov265@gmail.com>
2022-04-21 17:15:07 +0300

    add: GetPostFromIdPost

---
+++ NashGramBack/PostDB.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashGramBack
+{
+    internal class PostDB
+    {
+        private static string pathDB = System.Reflection.Assembly.GetExecutingAssembly().Location.ToString().Replace("NashGramBack.dll", "") + @"\sqlite\databaseNashGram.db";
+
+        /// <summary>
+        /// Получает Account по id
+        /// </summary>        
+        public static Post? GetPostFromIdPost(long id)
+        {
+            try
+            {
+                Post? post = null;
+                using (var connection = new SQLiteConnection(@$"Data Source={pathDB};Version=3;"))
+                {
+                    connection.Open();
+                    using (var cmd = new SQLiteCommand($"SELECT * FROM Post WHERE id_post = {id};", connection))
+                    {
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            post = new Post();
+
+                            while (reader.Read())
+                            {
+                                post.author = reader.GetInt64(1);
+                                post.uri = reader.GetString(2);
+                                post.descryption = reader.GetString(3);
+                                post.tag = reader.GetString(4);
+                            }
+                        }
+                    }
+                }
+                return post;
+            }
+            catch (Exception ex)
+            {
+                Log.AddLog($"Account not found from ID: {id} | " + ex.Message, true);
+                return null;
+            }
+        }
+    }
+}
