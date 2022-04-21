9ad145a288e41a180d251697174e90d54cce64d0
Valerij <shishov265@gmail.com>
2022-04-21 17:04:26 +0300

    add: Post class

---
+++ NashGramBack/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashGramBack
+{
+    public class Post
+    {
+        public long id { get; set; }
+        public long author { get; set; }
+        public string uri { get; set; }
+        public string descryption { get; set; }
+        public string tag { get; set; }
+    }
+}
