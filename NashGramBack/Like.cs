bca06240b69646af439906392c1027489cb764ed
Valerij <shishov265@gmail.com>
2022-04-21 17:25:31 +0300

    add: Like class

---
+++ NashGramBack/Like.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashGramBack
+{
+    public class Like
+    {
+        public long idPost { get; set; }
+        public long idAccount { get; set; }
+    }
+}
