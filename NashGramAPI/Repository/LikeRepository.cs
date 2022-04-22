5516d5780bdec94064a8ccd278eb7f41710fe4f6
Valerij <shishov265@gmail.com>
2022-04-23 01:45:24 +0300

    add: LikeRepository

---
+++ NashGramAPI/Repository/LikeRepository.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NashGramAPI.Model;
+
+namespace NashGramAPI.Repository;
+
+internal class LikeRepository
+{
+    private static string pathDB = ModelClass.pathDB;
+
+    /// <summary>
+    /// Получает Like по id_post
+    /// </summary>        
+    public static Like? GetLikeFromIdPost(long id)
+    {
+        try
+        {
+            Like? like = null;
+            using (var connection = new SQLiteConnection(@$"Data Source={pathDB};Version=3;"))
+            {
+                connection.Open();
+                using (var cmd = new SQLiteCommand($"SELECT * FROM Like WHERE id_post = {id};", connection))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        like = new Like();
+
+                        while (reader.Read())
+                        {
+                            like.id = reader.GetInt64(0);
+                            like.idPost = reader.GetInt64(1);
+                            like.idAccount = reader.GetInt64(2);  
+                        }
+                    }
+                }
+            }
+            return like;
+        }
+        catch (Exception ex)
+        {
+            Log.AddLog($"Like not found from ID: {id} | " + ex.Message, true);
+            return null;
+        }
+    }
+
+    public static Like? GetLikeFromId(long id)
+    {
+        try
+        {
+            Like? like = null;
+            using (var connection = new SQLiteConnection(@$"Data Source={pathDB};Version=3;"))
+            {
+                connection.Open();
+                using (var cmd = new SQLiteCommand($"SELECT * FROM Like WHERE id = {id};", connection))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        like = new Like();
+
+                        while (reader.Read())
+                        {
+                            like.id = reader.GetInt64(0);
+                            like.idPost = reader.GetInt64(1);
+                            like.idAccount = reader.GetInt64(2);
+                        }
+                    }
+                }
+            }
+            return like;
+        }
+        catch (Exception ex)
+        {
+            Log.AddLog($"Like not found from ID: {id} | " + ex.Message, true);
+            return null;
+        }
+    }
+
+    public static Like? GetLikeFromIdAccount(long id)
+    {
+        try
+        {
+            Like? like = null;
+            using (var connection = new SQLiteConnection(@$"Data Source={pathDB};Version=3;"))
+            {
+                connection.Open();
+                using (var cmd = new SQLiteCommand($"SELECT * FROM Like WHERE id_account = {id};", connection))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        like = new Like();
+
+                        while (reader.Read())
+                        {
+                            like.id = reader.GetInt64(0);
+                            like.idPost = reader.GetInt64(1);
+                            like.idAccount = reader.GetInt64(2);
+                        }
+                    }
+                }
+            }
+            return like;
+        }
+        catch (Exception ex)
+        {
+            Log.AddLog($"Like not found from ID: {id} | " + ex.Message, true);
+            return null;
+        }
+    }
+}
